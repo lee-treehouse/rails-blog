2d476ad9f49630cbc62fb80bc18aea20602d79d9
leentaylor <leentaylor@gmail.com>
2023-09-27 15:24:55 +1000

    add some extra context to my prior two steps

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  
+  # index is an action
+  #When an action does not explicitly render a view (or otherwise trigger an HTTP response), 
+  #Rails will automatically render a view that matches the name of the controller and action.
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # the articles route will go to the index action of the articles controller
   get "/articles", to: "articles#index"
 
   # Defines the root path route ("/")
