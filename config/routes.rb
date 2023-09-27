4d0e0e4dfc7d5643e80411769b808b866cd20cd7
leentaylor <leentaylor@gmail.com>
2023-09-27 15:30:08 +1000

    set the root route which is called root "/"

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Defines the root path route ("/")
+  root "articles#index"
+
   # the articles route will go to the index action of the articles controller
   get "/articles", to: "articles#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
