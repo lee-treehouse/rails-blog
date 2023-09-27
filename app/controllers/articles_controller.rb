53168bc0d055ddc814b78e2b11195da711b9265e
leentaylor <leentaylor@gmail.com>
2023-09-27 15:33:41 +1000

    add explanatory comment about auto loading

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,17 @@
+# notice how I didn't need to rquire or import Application Controller to inherit from it 
+
+=begin
+Application classes and modules are available everywhere, you do not need and should not load anything 
+under app with require. This feature is called autoloading, and you can learn more about it in 
+Autoloading and Reloading Constants.
+https://guides.rubyonrails.org/autoloading_and_reloading_constants.html
+
+  You only need require calls for two use cases:
+  
+  To load files under the lib directory.
+  To load gem dependencies that have require: false in the Gemfile.
+=end
+
 class ArticlesController < ApplicationController
   
   # index is an action
